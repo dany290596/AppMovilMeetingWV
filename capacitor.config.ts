49025b62cf1afb8633b383bff5811acf72cdbb66
Daniel Benito Rosales <proyectos3@crcdemexico.com.mx>
2024-04-05 08:44:48 -0600

    Se crear la arquitectura para ::: (AppMovilMeetingWV) - (versionName '3.0.4') - (versionCode 30000)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AppMovilMeetingWV',
+  appId: 'com.crcdemexico.WebVisitsMeeting',
+  appName: 'WebVisitsMeeting',
   webDir: 'www',
   server: {
     androidScheme: 'https'
